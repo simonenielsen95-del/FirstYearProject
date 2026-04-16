bda1fb3489762782e2b0d199818120c5834297de
Simone Nielsen <simonenielsen95@gmail.com>
2026-04-16 14:43:23 +0200

    Navigation for 1. sprint, done.

--- NEFAB/ViewModels/ContainerViewModel.cs
+++ NEFAB/ViewModels/ContainerViewModel.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using NEFAB.Commands;
+using System.Windows.Input;
+using NEFAB.Stores;
+using NEFAB.Services;
 
 namespace NEFAB.ViewModels
 {
     public class ContainerViewModel : BaseViewModel
     {
+        public ICommand NavigateToHomeViewCommand { get; }
+
+        public ContainerViewModel(NavigationStore navigationStore)
+        {
+            NavigationService homeNavigationService = new NavigationService(navigationStore, () => new HomeViewModel(navigationStore));
+
+            NavigateToHomeViewCommand = new NavigateCommand(homeNavigationService);
+        }
     }
 }
--- NEFAB/ViewModels/HomeViewModel.cs
+++ NEFAB/ViewModels/HomeViewModel.cs
@@ -4,6 +4,7 @@ using System.Text;
 using NEFAB.Commands;
 using System.Windows.Input;
 using NEFAB.Stores;
+using NEFAB.Services;
 
 namespace NEFAB.ViewModels
 {
@@ -11,10 +12,19 @@ namespace NEFAB.ViewModels
     public class HomeViewModel : BaseViewModel
     {
         public ICommand NavigateToContainerViewCommand { get; }
+        public ICommand NavigateToEmployeeViewCommand { get; }
+
+        public ICommand NavigateToSupplierViewCommand { get; }
 
         public HomeViewModel(NavigationStore navigationStore)
         {
-            NavigateToContainerViewCommand = new NavigateToContainerViewCommand(navigationStore);
+            NavigationService containerNavigationService = new NavigationService(navigationStore, ()=> new ContainerViewModel(navigationStore));
+            NavigationService employeeNavigationService = new NavigationService(navigationStore, () => new EmployeeViewModel(navigationStore));
+            NavigationService supplierNavigationService = new NavigationService(navigationStore, () => new SupplierViewModel(navigationStore));
+
+            NavigateToContainerViewCommand = new NavigateCommand(containerNavigationService);
+            NavigateToEmployeeViewCommand = new NavigateCommand(employeeNavigationService);
+            NavigateToSupplierViewCommand = new NavigateCommand(supplierNavigationService);
         }
     }
 }
