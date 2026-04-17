f82e1d3f4b9a241310dffe3b2755a6b75a19873f
Farishta Nuri <fanu74779@edu.ucl.dk>
2026-04-17 11:36:11 +0200

    Fortsættelse af EmployeeRepo

--- NEFAB/Repositories/EmployeeRepository.cs
+++ NEFAB/Repositories/EmployeeRepository.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using NEFAB.Domains;
+using Microsoft.Extensions.Configuration;
 
 namespace NEFAB.Repositories
 {
@@ -18,5 +22,60 @@ namespace NEFAB.Repositories
             Employees = new List<Employee>();
             ConnectionString = config.GetConnectionString("MyDBConnection");
         }
-    }
+
+        public List<Employee> GetAll()
+        {
+            //Her returner Employees der allerede ligger i databasen
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                string query = "SELECT EmployeeID, EmployeeName FROM Employees";
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    string employeeID = reader.GetString(0);
+                    string employeeName = reader.GetString(1);
+                    Employee employee = new Employee(employeeID)
+                    {
+                        EmployeeName = employeeName
+                    };
+                    Employees.Add(employee);
+                }
+            }
+            return Employees;
+        }
+
+        public void Add(Employee employee)
+        {
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO Employees (EmployeeID, EmployeeName) VALUES (@EmployeeID, @EmployeeName);", con))
+                {
+                    cmd.Parameters.AddWithValue("@EmployeeID", employee.EmployeeID);
+                    cmd.Parameters.AddWithValue("@EmployeeName", employee.EmployeeName);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            Employees.Add(employee);
+        }
+
+        public void RemoveAll(Employee employee)
+        {
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand("DELETE FROM Employees WHERE EmployeeID = @EmployeeID;", con))
+                {
+                    cmd.Parameters.AddWithValue("@EmployeeID", employee.EmployeeID);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            //Fjern employee fra den lokale liste, så den ikke længere vises i UI'et
+            Employees.RemoveAll(e=> e.EmployeeID == employee.EmployeeID);
+        }
+
+        
+        }
 }
