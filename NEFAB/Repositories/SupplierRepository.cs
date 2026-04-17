a23871e81f6c1959d09f2cc42e1d2e14fb3b7a2c
Simone Nielsen <simonenielsen95@gmail.com>
2026-04-17 11:43:03 +0200

    changed suplierRepo (in progress)

--- NEFAB/Repositories/SupplierRepository.cs
+++ NEFAB/Repositories/SupplierRepository.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Data;
+using System.Linq;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using NEFAB.Domains;
 
 namespace NEFAB.Repositories
 {
     public class SupplierRepository
     {
-    }
+        private readonly string ConnectionString;
+
+        private Supplier supplier;
+
+        public SupplierRepository()
+        {
+            IConfigurationRoot config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            ConnectionString = config.GetConnectionString("MyDBConnection");
+
+        }
+
+
+
+    }   
+
 }
