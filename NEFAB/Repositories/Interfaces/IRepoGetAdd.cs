1623eb9dad547a2734f461243b9051c8f584cb79
Simone Nielsen <simonenielsen95@gmail.com>
2026-04-21 13:20:53 +0200

    IRepo added and the repositories inharrense from IRepo

---
+++ NEFAB/Repositories/Interfaces/IRepoGetAdd.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NEFAB.Repositories.Interfaces
+{
+    public interface IRepoGetAdd<T,IDType>
+    {
+        /// <summary>
+        /// GetByID accepts the ID type specified when the interface is implemented
+        /// </summary>
+        T? GetByID(IDType id);
+        /// <summary>
+        /// Retuns a list with all items from the database table.
+        /// </summary>
+        List<T> GetAll();
+        /// <summary>
+        /// Add a new item to the database
+        /// </summary>
+        void Add(T item);
+
+    }
+}
