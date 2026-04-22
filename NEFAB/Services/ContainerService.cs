a6116fe6d747546b449e75c9657175e54caee199
Corinna Haustrup <corinnahaustrup@gmail.com>
2026-04-22 12:05:49 +0200

    databinding/commandbinding mere eller mindre færdigt, fortsætning af containerviewmodel

--- NEFAB/Services/ContainerService.cs
+++ NEFAB/Services/ContainerService.cs
@@ -3,19 +3,21 @@ using System.Collections.Generic;
 using System.Text;
 using NEFAB.Repositories;
 using NEFAB.Domains;
+using NEFAB.Repositories.Interfaces;
 
 namespace NEFAB.Services
 {
     public class ContainerService 
     {
-        private readonly ContainerRepository _containerRepository;
+        private readonly IRepoGetAddUpdateRemove<Container, string> _containerRepository;
 
-        public ContainerService()
+        public ContainerService(IRepoGetAddUpdateRemove<Container>)
         {
-            _containerRepository = new ContainerRepository(); 
+            _containerRepository = containerRepository(); 
         }
 
-        public void Add(string ContainerNo )
+        public void Add(Container container)
+
 
 
     }
--- NEFAB/ViewModels/ContainerViewModel.cs
+++ NEFAB/ViewModels/ContainerViewModel.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using NEFAB.Domains;
 using System.Text;
-using NEFAB.Commands;
+using System.Windows.Controls;
 using System.Windows.Input;
-using NEFAB.Stores;
+using NEFAB.Commands;
+using NEFAB.Repositories;
 using NEFAB.Services;
+using NEFAB.Stores;
+using NEFAB.Repositories.Interfaces;
 
 namespace NEFAB.ViewModels
 {
@@ -12,11 +16,30 @@ namespace NEFAB.ViewModels
     {
         public ICommand NavigateToHomeViewCommand { get; }
 
+        public ICommand UpdateContainerCommand { get; }
+        public ICommand RemoveContainerCommand { get; }
+        public ICommand CreateNewContainerCommand { get; }
+
+
+
         public ContainerViewModel(NavigationStore navigationStore)
         {
             NavigationService homeNavigationService = new NavigationService(navigationStore, () => new HomeViewModel(navigationStore));
 
             NavigateToHomeViewCommand = new NavigateCommand(homeNavigationService);
         }
+
+      
+        private ContainerRepository containerRepository = new ContainerRepository();
+
+        public string ContainerNo { get; set; }
+        public int Week { get; set; }
+        public int Year { get; set; }
+
+        public void Add(Container container)
+        { 
+            _container
+        }
     }
 }
+
