b4d1a8d0905e8a04692a5c042a3b6d8a5a046deb
Corinna Haustrup <corinnahaustrup@gmail.com>
2026-04-24 09:25:43 +0200

    cleanup employeeservice

--- NEFAB/Services/EmployeeService.cs
+++ NEFAB/Services/EmployeeService.cs
@@ -8,28 +8,21 @@ namespace NEFAB.Services
 {
     public class EmployeeService
     {
-      //  private readonly IRepoGetAddUpdate<Employee, string> _employeeRepository;
-
         public EmployeeRepository EmployeeRepository { get; set; }
-
         public EmployeeService()
         {
             EmployeeRepository = new EmployeeRepository();
         }
-
         public void Add(Employee employee)
         {
             if (string.IsNullOrEmpty(employee.EmployeeID) || (employee.EmployeeName == null))
             {
                 throw new ArgumentException("Udfyld venligst både EmployeeID og EmployeeName.");
             }
-
-
             if (employee.EmployeeID.Length != 8)
             {
                 throw new ArgumentException("EmployeeID skal være præcis 8 tegn langt.");
             }
-
             for (int i = 0; i < employee.EmployeeID.Length; i++)
             {
                 if (i <= 3)
@@ -46,35 +39,26 @@ namespace NEFAB.Services
                         throw new ArgumentException("De sidste 4 tegn skal være tal (0-9).");
                 }
             }
-
-                try
-                {
-                    EmployeeRepository.Add(employee);
-
-                }
-                catch
-                {
-                    throw new ArgumentException("Noget gik galt, prøv igen");
-                }
+            try
+            {
+                EmployeeRepository.Add(employee);
 
             }
-        
-             
-            
-        
-
+            catch
+            {
+                throw new ArgumentException("Noget gik galt, prøv igen");
+            }
+        }
         public void Update(Employee employee)
         {
             if (string.IsNullOrEmpty(employee.EmployeeID) || employee.EmployeeName == null)
             {
                 throw new ArgumentException("Udfyld venligst både EmployeeID, Nyt EmployeeID og EmployeeName.");
             }
-
             if (employee.EmployeeID.Length != 8)
             {
                 throw new ArgumentException("Udfyld venligst et korrekt employeeID");
             }
-
             for (int i = 0; i < employee.EmployeeID.Length; i++)
             {
                 if (i <= 3)
@@ -84,35 +68,33 @@ namespace NEFAB.Services
                     {
                         throw new ArgumentException("EmployeeID er ikke af korrekt type: ABCD1234");
                     }
-
                 }
                 else
                 {
-
                     if (!char.IsDigit(employee.EmployeeID[i]))
                     {
                         throw new ArgumentException("EmployeeID er ikke af korrekt type: ABCD1234");
                     }
                 }
-
-
-                    Employee? employeeDB = EmployeeRepository.GetByID(employee.EmployeeID);
-                    if (employeeDB == null)
-                    {
-                        throw new Exception($"Employee {employee.EmployeeID} blev ikke fundet.");
-                    }
-
-                        try
-                        {
-                            EmployeeRepository.Update(employee);
-                        }
-                        catch (Exception ex)
-                        {
-                            throw new Exception("Employee kunne ikke opdateres");
-                        }
-                    }
+                Employee? employeeDB = EmployeeRepository.GetByID(employee.EmployeeID);
+                if (employeeDB == null)
+                {
+                    throw new Exception($"Employee {employee.EmployeeID} blev ikke fundet.");
                 }
-
+                try
+                {
+                    EmployeeRepository.Update(employee);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Employee kunne ikke opdateres");
+                }
             }
         }
-    
+
+    }
+}
+
+
+
+
