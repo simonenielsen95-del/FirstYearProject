ce2f7823591cb02bfcf205849275487e14edb6cc
Simone Nielsen <simonenielsen95@gmail.com>
2026-04-25 15:15:02 +0200

    mad a package domain and removed some using driections that vas not being used.

--- NEFAB/Domains/Package.cs
+++ NEFAB/Domains/Package.cs
@@ -6,6 +6,17 @@ namespace NEFAB.Domains
 {
     public class Package
     {
-        
+        public int? PackageId { get; set; }
+        public int? ProjectNo { get; set; }
+        public int? ProjectItemNo { get; set; }
+        public int? PackageWeight { get; set; }
+        public int? Amount { get; set; }
+        public float? PackageLength {  get; set; }
+        public float? PackageWidth { get; set; }
+        public float? PackageHeight { get; set; }
+        public string? Comment { get; set; }
+
+
+
     }
 }
