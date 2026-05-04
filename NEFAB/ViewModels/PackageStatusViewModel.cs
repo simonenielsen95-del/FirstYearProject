794ab2cffecbeb631d7853c782d2cfcc2b22d40b
Corinna Haustrup <corinnahaustrup@gmail.com>
2026-05-04 10:38:18 +0200

    navigation virker nu

--- NEFAB/ViewModels/PackageStatusViewModel.cs
+++ NEFAB/ViewModels/PackageStatusViewModel.cs
@@ -12,6 +12,37 @@ namespace NEFAB.ViewModels
 {
     public class PackageStatusViewModel : BaseViewModel
     {
+        public ICommand NavigateToPackageViewCommand { get; }
+        public ICommand ChangeStatusPackageCommand { get; }
 
+        private readonly PackageService _packageService;
+
+        private Package _selectedPackage;
+        public Package SelectedPackage
+        {
+            get { return _selectedPackage; }
+            set { _selectedPackage = value; OnPropertyChanged(); }
+        }
+
+
+        public PackageStatusViewModel(NavigationStore navigationStore, Package selectedPackage)
+        {
+            NavigationService packageNavigationService = new NavigationService(navigationStore, () => new PackageViewModel(navigationStore));
+
+            NavigateToPackageViewCommand = new NavigateCommand(packageNavigationService);
+
+            ChangeStatusPackageCommand = new CommandHandler(() => ChangeStatusPackage());
+            _packageService = new PackageService();
+
+            SelectedPackage = new Package();
+
+            SelectedPackage = selectedPackage;
+        }
+
+        public void ChangeStatusPackage()
+        {
+
+        }
     }
 }
+
