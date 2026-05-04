cbb8daa4a24722ec330876ed9254c167931393e0
Corinna Haustrup <corinnahaustrup@gmail.com>
2026-05-04 10:20:25 +0200

    PackageStatusView generel design implementeret og opstart af packagestatusviewmodel

---
+++ NEFAB/ViewModels/PackageStatusViewModel.cs
@@ -0,0 +1,17 @@
+﻿using NEFAB.Commands;
+using NEFAB.Domains;
+using NEFAB.Services;
+using NEFAB.Stores;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace NEFAB.ViewModels
+{
+    public class PackageStatusViewModel : BaseViewModel
+    {
+
+    }
+}
