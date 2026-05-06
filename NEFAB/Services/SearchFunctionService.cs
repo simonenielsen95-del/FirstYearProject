0b820e1eb66d360150ed2d1e3f2f2d8a360a994a
Corinna Haustrup <corinnahaustrup@gmail.com>
2026-05-06 15:08:16 +0200

    opstart

---
+++ NEFAB/Services/SearchFunctionService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NEFAB.Services
+{
+    class SearchFunctionService
+    {
+     
+
+    }
+}
---
+++ NEFAB/ViewModels/SearchFunctionViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using NEFAB.Commands;
+using NEFAB.Domains;
+using NEFAB.Services;
+using NEFAB.Stores;
+
+namespace NEFAB.ViewModels
+{
+    class SearchFunctionViewModel : BaseViewModel
+    {
+        public ICommand NavigateToHomeViewCommand { get; }
+
+        private readonly SearchFunctionService _searchFunctionService;
+
+        public SearchFunctionViewModel(NavigationStore navigationStore)
+        {
+            NavigationService homenavigationService = new NavigationService(navigationStore, () => new HomeViewModel(navigationStore));
+            NavigateToHomeViewCommand = new NavigateCommand(homenavigationService);
+        }
+    }
+
+
+}
